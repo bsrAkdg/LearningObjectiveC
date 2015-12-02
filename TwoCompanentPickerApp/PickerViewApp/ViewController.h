a071aa42f1c6ec9dacd710f2f216e1b857292065
bsrAkdg <busraaakdag@gmail.com>
2015-12-02 10:42:09 +0200

    Companent yeniden yükleme, değiştirme, seçili satırları gösterme

--- TwoCompanentPickerApp/PickerViewApp/ViewController.h
+++ TwoCompanentPickerApp/PickerViewApp/ViewController.h
@@ -13,6 +13,7 @@
 @property (strong, nonatomic) IBOutlet UIPickerView *myPicker;
 @property NSArray *arr1;
 @property NSArray *arr2;
+- (IBAction)reloadPickerView:(id)sender;
 
 @end
 
--- TwoCompanentPickerApp/PickerViewApp/ViewController.m
+++ TwoCompanentPickerApp/PickerViewApp/ViewController.m
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    //şimdi iki companent' li pickerview yapıyoruz.
+    //şimdi iki component' li pickerview yapıyoruz.
 
     [myPicker setDelegate:self];
     [myPicker setDataSource:self];
@@ -31,6 +31,13 @@
     //şimdi ViewController.h'a bir dizi daha ekliyoruz: Ülke adlarını tutacak:
     arr2= [NSArray arrayWithObjects:@"İngiltere", @"Fransa", @"Amerika", @"İzlanda", nil ];
     
+    //hangi componentte hangi satırın seçili olarak geleceğini de belirtebiliriz: 0. componentte 3. satırı seçili olarak animasyonlu getir:
+    
+    [myPicker selectRow:3 inComponent:0 animated:YES];
+    
+    //1. componentte 2. satırı seçili olarak animasyonlu getir:
+    [myPicker selectRow:2 inComponent:1 animated:YES];
+
     
 }
 
@@ -39,28 +46,28 @@
     // Dispose of any resources that can be recreated.
 }
 
-//pickerview da kaç companent olacak: bizim örneğimizde iki olacak (isim-şehir):
+//pickerview da kaç component olacak: bizim örneğimizde iki olacak (isim-şehir):
 
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
     
-    return 1;
+    return 2;
 
 }
 
-//herbir companentte kaç satır var: bizim örneğimizde her companentin satır sayısı dizi uzunlukları kadar önce hangi companent olduğunu parametre olarak gelen "companent" ile kontrol ediyoruz :
+//herbir componentte kaç satır var: bizim örneğimizde her componentin satır sayısı dizi uzunlukları kadar önce hangi compaonent olduğunu parametre olarak gelen "component" ile kontrol ediyoruz :
 
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
     if(component == 0){
-        //isim companenti:
+        //isim componenti:
         return arr1.count;
     }
     else{
-        //ülke companenti:
+        //ülke componenti:
         return arr2.count;
     }
 }
 
-//her satır için dönecek değer: bize burda zaten parametre olarak hangi companent olduğu "companent" olarak göderiliyor o yüzden  hangi companentse ona ait diziden row indeksli objeyi çekeceğiz:
+//her satır için dönecek değer: bize burda zaten parametre olarak hangi component olduğu "component" olarak göderiliyor o yüzden  hangi componentse ona ait diziden row indeksli objeyi çekeceğiz:
 
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
     
@@ -73,4 +80,35 @@
     
 }
 
+//hangi satırı seçtiğimizi bize döndüren metod:
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    
+    //seçilen component ve satırı
+    NSLog(@" seçilen companent: %d , seçilen row: %d ", component, row);
+    
+    //0. componentte seçilen satırı görmek istiyorsam:
+    int secilensatir= [pickerView selectedRowInComponent:0]; // burada 0. companenti seçtiğimizi belirttik.
+    NSLog(@" 0. component te seçili satır: %d ", secilensatir);
+
+}
+
+//NSArray diyerek tanımlaıdğımız için biz şimdi statik olarak dizinin değeri değiştirip yeniden yüklemesini yapalım:
+- (IBAction)reloadPickerView:(id)sender {
+    arr1= @[@"buke", @"busra", @"emre", @"memo"];
+    
+    //burdapicker view ile ilgili bütün metodları yeniden çağrmasını sağlıyoruz: sil butonuna bastığı gibi buke, busra vs gösterilecek.
+    [myPicker reloadAllComponents];
+    
+    /*dilersek hangi componenti yeniden yükleyeceğinide belirtebiliriz:
+    
+    [myPicker reloadComponent:0]; //0. componenti yeniden yükle
+    */
+    
+    
+    //istersekte butona bastıktan sonra pickerview' i gizleyebiliriz:
+    
+    [myPicker setHidden:YES];
+    
+    
+}
 @end
